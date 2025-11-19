796786314d88c166d3b182add5f4d780407da04b
Sebastian Jung <sebi.iuvenis@gmail.com>
2025-11-19 16:23:47 +0100

    init

--- src/main/java/com/assignment4/tasks/LamportTimestamp.java
+++ src/main/java/com/assignment4/tasks/LamportTimestamp.java
@@ -3,20 +3,24 @@ package com.assignment4.tasks;
 
 
 public class LamportTimestamp {
+
     private int timestamp;
-    public LamportTimestamp(int time){
+
+    public LamportTimestamp(int time) {
 
         timestamp = time;
     }
-    public synchronized void tick(){
+
+    public synchronized void tick() {
         // TODO: update the timestamp by 1
     }
+
     public synchronized int getCurrentTimestamp(){
         return timestamp;
     }
-    public synchronized void updateClock(int receivedTimestamp){
+
+    public synchronized void updateClock(int receivedTimestamp) {
         // TODO: update the function to choose the higher value out of the two received timestamps
         timestamp = receivedTimestamp;
     }
-
 }
